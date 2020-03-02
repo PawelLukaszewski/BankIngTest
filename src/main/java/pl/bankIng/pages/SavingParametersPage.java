606e39a2534cefd091a465f45f048b2564fb5d80
pawellukaszewski <pawel.t.lukaszewski@gmail.com>
2020-03-02 21:23:14 +0100

    0.1

--- src/main/java/pl/bankIng/pages/SavingParametersPage.java
+++ src/main/java/pl/bankIng/pages/SavingParametersPage.java
@@ -1,4 +1,11 @@
 package pl.bankIng.pages;
 
+import org.openqa.selenium.support.PageFactory;
+import pl.SingletonWebDriver;
+
 public class SavingParametersPage {
+
+    public SavingParametersPage() {
+        PageFactory.initElements(SingletonWebDriver.getDriver(), this);
+    }
 }
